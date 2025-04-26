ca29a446f320762b731603951aa22bed4f430530
John Lois Floro <jlois.floro@gmail.com>
2025-04-26 15:36:23 +0800

    Add share links for opponent invitation

--- frontend/src/components/modal/share-modal/ShareModal.tsx
+++ frontend/src/components/modal/share-modal/ShareModal.tsx
@@ -1,10 +1,16 @@
-import { Dialog } from "radix-ui";
+import { Dialog, Tooltip } from "radix-ui";
 import { useSearchParams } from "react-router";
 import { useSocket } from "@stores/useSocket";
 import { useState } from "react";
 import { v4 } from "uuid";
 import modal from "../Modal.module.css";
 import share from "./ShareModal.module.css";
+import {
+    IconBrandTelegram,
+    IconBrandWhatsapp,
+    IconBrandX,
+    IconMail,
+} from "@tabler/icons-react";
 
 const className: CSSModuleClasses = {};
 Object.assign(className, share, modal);
@@ -15,6 +21,12 @@ export default function ShareModal() {
     const [roomId] = useState<string>(v4());
     const [playerId] = useState<string>(v4());
     const socket = useSocket((state) => state.socket);
+
+    const [isCopied, setIsCopied] = useState(false);
+
+    const shareLink = new URL(
+        `${import.meta.env.VITE_SITE_URL}/?o=${playerId}%26s=${roomId}%26p=${opponentId}`
+    );
 
     const handleInvitation = () => {
         setSearchParams((prev) => [
@@ -29,6 +41,16 @@ export default function ShareModal() {
             player: playerId,
             opponent: opponentId,
         });
+    };
+
+    const handleCopyShareLink = () => {
+        navigator.clipboard.writeText(shareLink.toString());
+
+        setIsCopied(true);
+
+        setTimeout(() => {
+            setIsCopied(false);
+        }, 3000);
     };
 
     return (
@@ -56,8 +78,102 @@ export default function ShareModal() {
                             <img src="/icon-close.svg" alt="Icon Close" />
                         </Dialog.Close>
                     </div>
-                    <div className={className.share__img}>
-                        {`${import.meta.env.VITE_SITE_URL}/?o=${playerId}&s=${roomId}&p=${opponentId}`}
+                    <div className={className.share__links}>
+                        <div className={className.share__icons}>
+                            <a
+                                href={`https://t.me/share/url?url=${shareLink}&text=Play%20Rock%20Paper%20Scissors`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className={`${className.share__link__item} ${className.share__link__telegram}`}
+                            >
+                                <div>
+                                    <IconBrandTelegram
+                                        className={className.share__icon}
+                                    />
+                                </div>
+                            </a>
+                            <a
+                                href={`https://wa.me/?text=${shareLink}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className={`${className.share__link__item} ${className.share__link__whatsapp}`}
+                            >
+                                <div>
+                                    <IconBrandWhatsapp
+                                        className={className.share__icon}
+                                    />
+                                </div>
+                            </a>
+                            <a
+                                href={`https://x.com/intent/post?text=Play%20Rock%20Paper%20Scissors!%20${shareLink}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className={`${className.share__link__item} ${className.share__link__twitter}`}
+                            >
+                                <div>
+                                    <IconBrandX
+                                        className={className.share__icon}
+                                    />
+                                </div>
+                            </a>
+                            <a
+                                href={`mailto:?subject=Play%20Rock%20Paper%20Scissors!&body=A%20user%20invited%20you%20play%20Rock%20Paper%20Scissors%20on%20this%20link!%0A%0A${shareLink}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className={`${className.share__link__item} ${className.share__link__mail}`}
+                            >
+                                <div>
+                                    <IconMail
+                                        className={className.share__icon}
+                                    />
+                                </div>
+                            </a>
+                        </div>
+                        <div className={className.share__link}>
+                            <label
+                                className={className.share__link__label}
+                                htmlFor="shareLink"
+                            >
+                                or copy this link
+                            </label>
+                            <div className={className.share__link__container}>
+                                <input
+                                    type="text"
+                                    name="shareLink"
+                                    className={className.share__link__input}
+                                    value={shareLink.toString()}
+                                />
+                                <Tooltip.Provider>
+                                    <Tooltip.Root open={isCopied}>
+                                        <Tooltip.Trigger asChild>
+                                            <button
+                                                onClick={handleCopyShareLink}
+                                                className={
+                                                    className.share__link__btn
+                                                }
+                                            >
+                                                Copy
+                                            </button>
+                                        </Tooltip.Trigger>
+                                        <Tooltip.Portal>
+                                            <Tooltip.Content
+                                                className={
+                                                    className.tooltip__content
+                                                }
+                                                sideOffset={5}
+                                            >
+                                                Link Copied
+                                                <Tooltip.Arrow
+                                                    className={
+                                                        className.tooltip__arrow
+                                                    }
+                                                />
+                                            </Tooltip.Content>
+                                        </Tooltip.Portal>
+                                    </Tooltip.Root>
+                                </Tooltip.Provider>
+                            </div>
+                        </div>
                     </div>
                     <Dialog.Close className={className.share__close__mobile}>
                         <img src="/icon-close.svg" alt="Icon Close" />
